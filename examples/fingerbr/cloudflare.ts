0c485d027dbeb767a52b18160022d40bec7167d1
JiangJianYi <jiangjianyi@gnlab.com>
2024-05-08 15:58:10 +0800

    clean code

--- examples/fingerbr/cloudflare.ts
+++ examples/fingerbr/cloudflare.ts
@@ -1,29 +1,34 @@
 import { chromium, firefox, webkit } from "playwright";
 
-const userAgent =
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
-
 (async () => {
   const browser = await chromium.launch({
     slowMo: 500,
     headless: false,
+    chromiumSandbox: true,
+    ignoreDefaultArgs: true,
+    args: [],
   }); // Or 'firefox' or 'webkit'.
   const page = await browser.newPage({
-    userAgent,
     screen: {
       width: 1920,
       height: 1200,
     },
+    viewport: {
+      width: 1920,
+      height: 1200,
+    },
+    ignoreHTTPSErrors: true,
+    bypassCSP: true,
+    acceptDownloads: true,
+    deviceScaleFactor: 1,
+    isMobile: false,
+    hasTouch: false,
   });
 
   page.setDefaultTimeout(180_000);
   page.setDefaultNavigationTimeout(180_000);
 
-  page.addInitScript(() => {
-    Object.defineProperty(navigator, "webdriver", { get: () => undefined });
-  });
-
-  await page.goto("https://artio.faucet.berachain.com/");
+  page.goto("https://artio.faucet.berachain.com/");
 
   // other actions...
   // await browser.close();
